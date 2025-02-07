0920e48c56010c9d149f62bd0e218c2e62ed8473
MuVer <82942543+Murad-big@users.noreply.github.com>
2025-02-07 21:54:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from handlers import (
     unknown_command, user_selection, task_selection,
     update_status, delete_task)
 
+from google_sheet_handler import move_ideas, notify_responsible, handle_message
+
 from datetime import datetime
 
 
@@ -22,6 +24,10 @@ def run_bot2():
     initialize_excel()
 
     application = Application.builder().token(TOKEN).build()
+    job_queue = application.job_queue
+
+    job_queue.run_repeating(move_ideas, interval=60, first=0)
+    job_queue.run_repeating(notify_responsible, interval=60, first=10)
 
     scheduler = BackgroundScheduler()
     scheduler.add_job(send_reminders_sync, "interval", days=3, args=[application], next_run_time=datetime.now())
@@ -33,11 +39,14 @@ def run_bot2():
     application.add_handler(CommandHandler("check_deadline", check_deadline))
     application.add_handler(CallbackQueryHandler(user_selection, pattern=r"^user:"))
     application.add_handler(CallbackQueryHandler(task_selection, pattern=r"^task:"))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, unknown_command))
+    application.add_handler(MessageHandler(filters.ALL, handle_message))
     application.add_handler(MessageHandler(filters.COMMAND, unknown_command))
+
     application.add_handler(CallbackQueryHandler(update_status, pattern=r"^status:"))
     application.add_handler(CallbackQueryHandler(delete_task, pattern=r"^delete:"))
 
     application.run_polling()
+
+
 if __name__ == "__main__":
     run_bot2()
